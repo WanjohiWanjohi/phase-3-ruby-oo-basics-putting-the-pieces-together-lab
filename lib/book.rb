ba0d5a9dece7d7a909701b4ca7b1ff13c990d0a0
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-08-24 21:33:18 +0300

    commit tree cdaf6b4ad23eede1209ac5b1787b397e1266fbaa

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
-
+class Book
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+    
+    def initialize(title)
+        @title = title
+    end
+    def turn_page
+        @page_count = (@page_count + 1)
+    end
+end
 
